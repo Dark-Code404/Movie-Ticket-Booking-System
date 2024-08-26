6b43a5a486dbb74de58ee2045617aa0fdb762f1f
Dark-Code404 <luiyunish@gmail.com>
2024-08-26 16:53:05 +0545

    final

--- MT_main/admin.py
+++ MT_main/admin.py
@@ -3,19 +3,11 @@ from django.contrib import admin
 from MT_main.models import *
 
 
-class Movies_actors_filter(admin.ModelAdmin):
-    
-  
-    def get_form(self, request, obj=None, **kwargs):
-        form = super().get_form(request, obj, **kwargs)
-        
-       
-        form.base_fields['actors'].queryset = Actor.objects.filter(movies__isnull=True)
-        return form
+
 
 
 admin.site.register(Cinemas)
-admin.site.register(Movies,Movies_actors_filter)
+admin.site.register(Movies)
 admin.site.register(Screening)
 admin.site.register(Bookings)
 admin.site.register(ShowTime)
